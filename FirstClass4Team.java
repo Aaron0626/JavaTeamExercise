14d773aa2685ac986f715e2c244e9fcf1729bb07
Albert Ma <shangpinma@gmail.com>
2025-04-16 10:35:19 +0800

    feat: first version-print the first member name

---
+++ FirstClass4Team.java
@@ -0,0 +1,6 @@
+public class FirstClass4Team {
+    public static void main(String[] args) {
+        System.out.println("這是用來練習GitHub協作的程式!");
+        System.out.println("\"學號：\"12345678\"\n姓名：\"谷翔平\"");        
+    }
+}
