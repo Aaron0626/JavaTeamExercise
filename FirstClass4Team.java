cb148f1341a845780d997bcfc20d2897448d896b
lyl <rainyseasonmail@gmail.com>
2025-04-23 13:31:46 +0800

    Update FirstClass4Team.java

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -2,7 +2,7 @@ public class FirstClass4Team {
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"12345678\"\n姓名：\"谷翔平\"");   
-        System.out.println("\"學號：\"23456789\"\n姓名：\"申山本\""); 
+        System.out.println("\"學號：\"01257054\"\n姓名：\"陳敏瑄\""); 
         System.out.println("\"學號：\"34567890\"\n姓名：\"左佐牧\""); 
     }
 }
