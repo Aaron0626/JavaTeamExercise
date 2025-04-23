7b8b85f58c3aaa9ea211831fa9683e08662e320c
lyl <rainyseasonmail@gmail.com>
2025-04-23 13:44:34 +0800

    01257054

--- FirstClass9Team.java
+++ FirstClass9Team.java
@@ -2,7 +2,7 @@ public class FirstClass9Team {
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01257129\"\n姓名：\"吳紀潓\"");   
-        System.out.println("\"學號：\"01257054 \"\n姓名：\"陳敏瑄\""); 
+        System.out.println("\"學號：\"01257054\"\n姓名：\"陳敏瑄\""); 
         System.out.println("\"學號：\"34567890\"\n姓名：\"左佐牧\""); 
     }
 }
