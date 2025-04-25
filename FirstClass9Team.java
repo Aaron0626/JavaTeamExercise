3ee6927af88e588bb68b4876e96ffff46f9a2047
Pei-0923 <151004666+Pei-0923@users.noreply.github.com>
2025-04-25 11:01:26 +0800

    Update FirstClass9Team.java

--- FirstClass9Team.java
+++ FirstClass9Team.java
@@ -3,6 +3,6 @@ public class FirstClass9Team {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01257129\"\n姓名：\"吳紀潓\"");   
         System.out.println("\"學號：\"01257054\"\n姓名：\"陳敏瑄\""); 
-        System.out.println("\"學號：\"34567890\"\n姓名：\"左佐牧\""); 
+        System.out.println("\"學號：\"01257165\"\n姓名：\"林浿錞\""); 
     }
 }
